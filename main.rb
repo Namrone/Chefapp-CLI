2a917c5e471d5b80ab10868987946e253af40965
Norman Cheng <151798569+Namrone@users.noreply.github.com>
2025-05-06 21:12:00 +0000

    Created prompt_add and moved all user interactions from add_item there. untested

--- main.rb
+++ main.rb
@@ -1,29 +1,78 @@
 require_relative "menu"
 
-menu = Menu.new
-input = 0
-
-while true do # Prints option menu and loops until user input is a valid input
-  puts "Please choose an option (#1-5):"
-  puts "1. Print Menu"
-  puts "2. Add an item"
-  puts "3. Remove an item"
-  puts "4. Edit an item"
-  puts "5. Exit"
-  input = gets.chomp.to_i
-
-  case input
-  when 1
+def prompt_add(menu)
+  response = ''
+  while response != 'y' && response != 'yes' do #Gets menu item information from user
+    puts "Please enter item name"
+    name = gets.chomp.titleize
+    return if name == 'Exit'
+    puts "Please enter item description"
+    description = gets.chomp.capitalize
+    return if description == 'Exit'
+    puts "Please enter price of item"
+    price = gets.chomp
+    return if price.downcase == 'exit'
+    price = price.to_f.ceil(2)
     
-  when 2
+    puts "Is this correct (Y)es or (N)o. Type 'exit' to return to main menu: \nName: #{name} \nDescription: #{description} \nPrice: #{price}"
+    
+    response = gets.chomp.downcase
+    return if response == 'exit'
+  end
 
-  when 3
+  loop do #Prompts user to choose a category and stores the item into it
+    puts "Would you like to: \n1. Enter the item into an existing category \n2. Add a new category"
+    choice = gets.chomp
+    return if choice.downcase = 'exit'
 
-  when 4
+    case
+    when choice == '1' && !menu.category.empty? #Prompts user to choose from exisiting categories and adds item into it
+      puts "Please choose from the following existing categories: #{@category.keys}"
+      cat_name = gets.chomp.titleize
+      return if cat_name == 'Exit'
 
-  when 5
-    break
-  else
-    puts "Not a vlaid option, please enter a value 1-5"
+      if menu.category.has_key?(cat_name)
+        menu.add_item(name, description, price, cat_name)
+      end
+    when choice == '2' #Prompts user to start new category and initializes the item into it
+      print "Please enter the new category name: "
+      cat_name = gets.chomp.titleize
+      return if cat_name == 'Exit'
+      if !menu.category.has_key?(cat_name)
+        menu.add_item(name, description, price, cat_name)
+      end
+    end
+    puts "Invalid Option, please try again."
   end
 end
+
+def start_menu(menu)
+  loop do # Prints option menu and loops until user input is a valid input
+    puts "Please choose an option (#1-5). You may type 'exit' anytime to return to main menu:"
+    puts "1. Print Menu"
+    puts "2. Add an item"
+    puts "3. Remove an item"
+    puts "4. Edit an item"
+    puts "5. Exit"
+    input = gets.chomp.to_i
+
+    case input
+    when 1
+      
+    when 2 #Add item to menu
+      prompt_add(menu)
+    when 3
+
+    when 4
+
+    when 5
+      return
+    else
+      puts "Not a valid option, please enter a value 1-5"
+    end
+  end
+end
+
+menu = Menu.new
+
+start_menu(menu)
--- menu.rb
+++ menu.rb
@@ -1,58 +1,15 @@
 class Menu
-  attr_accessor :menu_item
-  attr_accessor :name
-  attr_accessor :description
-  attr_accessor :price
-  attr_accessor :category
+  attr_accessor :name, :description, :price, :category
 
   def initializer
-    @menu_item = Hash.new
     @category = Hash.new
-    @price = 0
   end
 
-  def add_item 
-    response = ''
-    while response != 'y' || response != 'yes' do #Gets menu item information from user
-      puts "Please enter item name"
-      @name = gets.chomp
-      puts "Please enter item description"
-      @description = gets.chomp
-      puts "Please enter price of item"
-      @price = gets.chomp
-      
-      puts "Is this correct ((Y)es or (N)o)? \nName: #{@name} \nDescription: #{description} \nPrice: #{@price}"
-      
-      response = gets.chomp.downcase
-    end
-
-    while true do #Prompts user to choose a category and stores the item into it
-      if !@category.empty?
-        puts "Would you like to: \n1. Enter the item into an existing category \n2. Add a new category"
-        choice = gets.chomp.to_i
-
-        case choice
-        when 1 #To enter item into an existing category
-          puts "Please choose from the following existing categories: #{@category.keys}"
-          cat_name = gets.chomp.titleize
-          if @category.has_key?(cat_name) 
-            @category[cat_name] << {:item => @item, :description => @description, :price => @price}
-            break
-          end
-        end
-        when 2 #To add a new category and add item into it
-          print "Please enter the new category name: "
-          cat_name = gets.chomp.titleize
-          if !@category.has_key?(cat_name)
-            @category[cat_name] = [{:item => @item, :description => @description, :price => @price}]
-          end
-          break
-        end
-
-        puts "Invalid Option. Please try again"
-      end
-      puts "What would you like to name your category?(ie. Appetizer, Dinner, Salads, Drinks)"
-      
+  def add_item (name, description, price, cat_name) #Adds the new menu item into corresponding category or forms new category
+    if @category.has_key?(cat_name)
+      @category[cat_name] << {:item => item, :description => description, :price => price}
+    else
+      @category[cat_name] = [{:item => item, :description => description, :price => price}]
     end
   end
 
@@ -69,7 +26,7 @@ class Menu
   end
 
   def print_menu
-
+    @category
   end
 
   def print_edit_choice
